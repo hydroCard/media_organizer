53400fbdaa2f20c4edd611ed5dd928952d5c8c49
--global <--global>
2021-08-19 17:27:28 -0600

    progress made; fixed orientation

--- main.sh
+++ main.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 #
 #  0) Create backupMediaScan folder on ~/Desktop
@@ -22,39 +23,38 @@ cd "$disk0"
 # Below is the start of the logic for if more than one drive is connected
 #read -r disk0 disk1 disk2 <<< $drive_root"$(df -h | grep NIKON | cut -d "/" -f 5)"
 #    b. If only one connected device, select that, if more than one, prompt user to select
-#
-#
 #    c. Copy folder(s) /DCIM/851ND600 ("DSC_0001.JPG" is the image filename) to ~/Desktop/backupMediaScan/
 #        i. there can be multiple XYZND*00 folders within DCIM
 #image_folder0="$(ls | awk '/[0-9]{3}ND[0-9]{1}00/{print $0}')"
-read -r image_folder0 image_folder1 image_folder2 <<< "$(ls | awk '/[0-9]{3}ND[0-9]{1}00/{print $0}')"
+folders=()
+IFS=$'\n' folders=$(ls -1)
+#read -r image_folder0 image_folder1 image_folder2 <<< "$(ls | awk '/[0-9]{3}ND[0-9]{1}00/{print $0}')"
 #
 #
-#######################################################################
-# ***  Need a while true loop to account for more than one folder *** #
-#######################################################################
 #
-cp "$disk0"/"$image_folder0" ~/Desktop/backupMediaScan/ && cd ~/Desktop/backupMediaScan/
+for i in "${folders[@]}" ; do cp "$i" ~/Desktop/backupMediaScan/; done
 #
-# 
-# 2) Loop through all files within available folders 
-for f in ./*.jpg
+cd ~/Desktop/backupMediaScan/
+#
+#
+# 2) Loop through all files within available folders
+for f in *
 do
-  r=$(identify -format '%[fx:(h>w)]' "$f")
+  ORIENTATION=$(identify -format %[orientation] "$f")> /dev/null
 #   a. if next file is portrait,
-  if [[ r -eq 1 ]] 
+  if [[ "$ORIENTATION" == "LeftBottom" ]]
   then
-#      mv "$f" /path/to/portraits   <-- came with the code snippet
+#     mv "$f" /path/to/portraits   <-- came with the code snippet
 #
 #     i. open image immediately after portrait in preview, store path to file in $first_image_for_current_trip
-      
-#     ii. extract date and time from $first_image_for_current_trip and store in $first_image_for_current_trip_date and $first_image_for_current_trip_time respectively 
+
+#     ii. extract date and time from $first_image_for_current_trip and store in $first_image_for_current_trip_date and $first_image_for_current_trip_time respectively
 #       - if < 1200, $am_or_pm = "AM"
 #       - otherwise, $am_or_pm = "PM"
-#     iii. prompt user for outfitter, store user input in $current_outfitter variable 
-#     iv. use "$first_image_for_current_trip_date + $current_outfitter + $location + $am_or_pm + $first_image_for_current_trip_time" for folder name, store in $folder_name variable 
+#     iii. prompt user for outfitter, store user input in $current_outfitter variable
+#     iv. use "$first_image_for_current_trip_date + $current_outfitter + $location + $am_or_pm + $first_image_for_current_trip_time" for folder name, store in $folder_name variable
   else
-      mv "$f" /path/to/landscapes
+#      mv "$f" /path/to/landscapes  <-- came with the code snippet
 #   b. if next file is landscape,
 #     i. copy file to the newly created folder $folder_name
   fi
